3d629c6609a59f16ca0433462dfb3fe159dfed14
Daniel Sokil <dtechtours@gmail.com>
2021-07-09 12:43:04 -0400

    Calculator UI

--- public/calculator.js
+++ public/calculator.js
@@ -1,1 +1,6 @@
-export default {}
+function solveEasy(string) {
+    // Is This Cheating?
+    return Number(eval(string))
+}
+
+export default { solve: solveEasy }
--- public/index.js
+++ public/index.js
@@ -1,6 +1,79 @@
 import calculator from "./calculator.js"
 
-function showResponse(output) {
+function renderOutput(output) {
+    const operationSymbols = {
+        "/": "&#247;",
+        "*": "&#215;",
+        "-": "&#8722;",
+        "+": "&#43;",
+    }
+
     document.querySelector("#calculator-response-pane").textContent =
         String(output)
 }
+
+function initializeCalculator() {
+    let currentValue = ""
+    let lastCommand = null
+    const lastCharIsOperationRegex = /([\/|\*|\-|\+])$/
+
+    const calculatorItems = Array.from(
+        document.querySelectorAll(".calculator-item"),
+    )
+
+    calculatorItems.forEach((calculatorItem) => {
+        const itemDataset = calculatorItem.dataset
+        const itemValue = calculatorItem.dataset.value
+        calculatorItem.textContent = itemValue
+
+        calculatorItem.onclick = () => {
+            if ("operation" in itemDataset) {
+                // Operation Cannot Be First
+                if (!currentValue) return
+
+                let operation = itemDataset.operation
+
+                if (operation === "equals") {
+                    if (currentValue.match(lastCharIsOperationRegex)) {
+                        // Operation Cannot Be Last
+                        return
+                    }
+
+                    currentValue = calculator.solve(currentValue)
+                    renderOutput(currentValue)
+                    return
+                }
+
+                const operationsCommand = {
+                    division: "/",
+                    multiplication: "*",
+                    subtraction: "-",
+                    addition: "+",
+                }
+
+                const command = operationsCommand[operation]
+                if (Object.values(operationsCommand).includes(lastCommand)) {
+                    // Cannot Have More Than One Operation
+                    currentValue = currentValue.replace(
+                        lastCharIsOperationRegex,
+                        command,
+                    )
+                    lastCommand = command
+                    renderOutput(currentValue)
+                    return
+                }
+
+                currentValue += command
+                lastCommand = command
+                renderOutput(currentValue)
+            } else {
+                if (isNaN(itemValue)) return
+
+                currentValue += itemValue
+                lastCommand = itemValue
+                renderOutput(currentValue)
+            }
+        }
+    })
+}
+initializeCalculator()
